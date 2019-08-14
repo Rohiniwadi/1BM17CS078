ab72b9c2a908411c7217a9498c18400720b5869a
Rohiniwadi <53818416+Rohiniwadi@users.noreply.github.com>
2019-08-14 12:35:35 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -6,15 +6,16 @@ def sqroot(n):
    while l<=h:
       m=(h+l)/2
       if m*m==n:
-         ans=m
+         ans=int(m+1)
          break
       elif m*m<n:
          l=m+1
-         ans=m
-      elif m*m>n:
+         ans=int(m+1)
+      else:
          h=m-1
    return ans
-print(sqroot(10))
-print(sqroot(-12))
-print(sqroot(0))
+n=int(input())
+print(sqroot(n))
+
+
 
