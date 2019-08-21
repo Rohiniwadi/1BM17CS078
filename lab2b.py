940a8447d47a6e6bde2171ec4e1b108964de801a
Rohiniwadi <53818416+Rohiniwadi@users.noreply.github.com>
2019-08-21 13:10:28 +0530

    Update lab2b.py

--- lab2b.py
+++ lab2b.py
@@ -3,7 +3,7 @@ def largest_element(a,k):
         for j in range(len(a)-i-1):
             if(a[j]>a[j+1]):
                 a[j],a[j+1]=a[j+1],a[j]
-    print(a[len(a)-k])
+    print(a[-k:])
        
 a=list(map(int,input().split()))
 k=int(input())
